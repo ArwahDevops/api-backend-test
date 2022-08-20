359c4d6788aacaed96c86765441aeb5d1c3a82e3
Riki Permana <dwi@neuronworks.co.id>
2022-08-20 13:02:20 +0700

    remove setup.sh, move to README.md

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Setup docker network & volumes
-docker network create environment-development
-# volume backend 1
-docker volume create backend-1-vol-development
-# volume backend 2
-docker volume create backend-2-vol-development
